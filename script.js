492d73ca4ab5ae99acf4925c19e6cb889763fa80
devCadet <satyam.karn3629@gmail.com>
2023-06-17 21:44:46 +0530

    added function to display typing animation

--- script.js
+++ script.js
@@ -12,6 +12,26 @@ const createElement = (html, className) => {
     return chatDiv;  // returning the created chat div
 }
 
+// function to show typing animation 
+const showTypingAnimation = () => {
+    const html = `<div class="chat-content">
+                        <div class="chat-details">
+                            <img src="./asset/chatbot.jpg" alt="chatbot-img">  
+                            <div class="typing-animation">
+                                <div class="typing-dot" style="--delay: 0.2s"></div>
+                                <div class="typing-dot" style="--delay: 0.3s"></div>
+                                <div class="typing-dot" style="--delay: 0.4s"></div>
+                            </div>
+                        </div>
+                        <span class="material-symbols-rounded">content_copy</span>
+                    </div>`;
+
+        // create an incoming chat div with typing animation and append it to chat container 
+    const outgoingChatDiv = createElement(html, "incomingChatDiv")
+    chatContainer.appendChild(outgoingChatDiv)
+}
+
+
 const handleOutgoingChat = () => {
     userText = chatInput.value.trim();
     const html = `<div class="chat-content">
@@ -24,6 +44,9 @@ const handleOutgoingChat = () => {
         // create an outgoing chat div with user's message and append it to chat container 
     const outgoingChatDiv = createElement(html, "outgoing")
     chatContainer.appendChild(outgoingChatDiv)
+
+    // showing animation for 3 dots after sending a message
+    setTimeout(showTypingAnimation, 500)
 
 
 }
